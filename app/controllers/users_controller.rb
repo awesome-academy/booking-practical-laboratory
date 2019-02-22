7c80d8aef93ce723b40f2142bc67690e7e3cb9a0
mabugay1234 <mabugay1234@gmail.com>
2019-02-22 13:19:23 +0700

    list user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,45 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :find_user, only: %i(show destroy)
+
+  def index
+    @users = User.page(params[:page]).per Settings.page_limit
+  end
+
+  def show
+    return if @user.present?
+    redirect_to users_path
   end
 
   def create
     @user = User.new user_params
 
-    if @user.save
-      log_in @user
-      flash[:success] = t ".content_users"
-      redirect_to @user
+    if @user
+      flash[:success] = t ".controller.find_success"
     else
-      flash[:notice] = t ".content_errousers"
-      render :new
+      flash[:danger] = t ".controller.find_danger"
+      redirect_to root_path
     end
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-
-    return if @user
-    flash[:danger] = t ".content_errousers"
-    redirect_to home_path
+  def destroy
+    if @user&.destroy
+      flash[:success] = t ".controller.destroy_success"
+    else
+      flash[:danger] = t ".controller.destroy_danger"
+    end
+    redirect_to users_url
   end
 
   private
+
+  def find_user
+    if @user = User.find_by(id: params[:id])
+      flash[:success] = t ".controller.find_success"
+    else
+      flash[:danger] = t ".controller.find_danger"
+      redirect_to root_path
+    end
+  end
 
   def user_params
     params.require(:user)
